dcb5d8a443c217d6ef65953a64ab99869c94af7f
hyonna12 <hyonna01@gmail.com>
2022-12-13 13:45:15 +0900

    기초세팅

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:chatting/firebase_options.dart';
+import 'package:chatting/pages/message_list_screen/message_list_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MessageListScreen(),
+    );
+  }
+}
